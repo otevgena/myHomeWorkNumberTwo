68b94bd03a434a0fa91bef134b369e1dc3830275
Evgenii Popov <saip0904@gmail.com>
2020-08-20 10:24:49 +0500

    Не совсем разобрался с Autolayout, и не получалось значения элементов StackView увидеть на симуляторе

--- myHomeWorkNumberTwo/ViewController.swift
+++ myHomeWorkNumberTwo/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLampLabel: UILabel!
+    @IBOutlet var yellowLampLabel: UILabel!
+    @IBOutlet var greenLampLavel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLampLabel.alpha = 1
+        yellowLampLabel.alpha = 0.3
+        greenLampLavel.alpha = 0.3
+        
+        redLampLabel.layer.masksToBounds = true
+        yellowLampLabel.layer.masksToBounds = true
+        greenLampLavel.layer.masksToBounds = true
+        
+        showTextButton.layer.cornerRadius = 10
+        redLampLabel.layer.cornerRadius = 40
+        yellowLampLabel.layer.cornerRadius = 40
+        greenLampLavel.layer.cornerRadius = 40
     }
-
-
+    
+    @IBAction func turningLightButton() {
+        
+        if redLampLabel.alpha == 1 {
+            redLampLabel.alpha = 0.3
+            yellowLampLabel.alpha = 1
+            showTextButton.setTitle("Next", for: .normal)
+            return
+        }
+        if yellowLampLabel.alpha == 1 {
+            yellowLampLabel.alpha = 0.3
+            greenLampLavel.alpha = 1
+            return
+        }
+        if greenLampLavel.alpha == 1 {
+            greenLampLavel.alpha = 0.3
+            redLampLabel.alpha = 1
+            showTextButton.setTitle("START", for: .normal)
+        }
+    }
 }
-
